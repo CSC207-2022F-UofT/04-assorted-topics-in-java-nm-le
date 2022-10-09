1a2a09b7ed28830759f3f30a3049cb92766b6dbd
Minh Le <ngocminhle318@gmail.com>
2022-10-09 15:30:22 -0400

    Submit

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -22,13 +22,19 @@ class DrivableMap {
         drivable_map = new HashMap<>();
     }
 
+
     /* TODO: Write a method named addDrivable that takes a String (the ID)
      *       and a Drivable object. If the ID string does not appear as a key
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
+    public boolean addDrivable(String ID, Drivable animal) {
+        if (drivable_map.containsKey(ID)) {
+            return false;
+        }
+        this.drivable_map.put(ID, animal);
+        return true;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -37,17 +43,28 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
+    public boolean hasFasterThan(int speed) {
+        for (Drivable animal : this.drivable_map.values()) {
+            if (animal.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
-
-
+    public List<Tradable> getTradable() {
+        ArrayList tradeable = new ArrayList<Tradable>();
+        for (Drivable animal : this.drivable_map.values()) {
+            if (animal instanceof Tradable) {
+                tradeable.add(animal);
+            }
+        }
+        return tradeable;
+    }
     
 }
--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -39,16 +39,20 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
-
-
+    public Trader(int money) {
+        this.money = money;
+        this.inventory = new ArrayList<T>();
+        this.wishlist = new ArrayList<T>();
+    }
 
 
 
     /* TODO: Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
-
-
+    public void addToWishlist(T item) {
+        this.wishlist.add(item);
+    }
 
 
 
@@ -59,8 +63,12 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
-
-
+    public int getSellingPrice(T item) {
+        if (item instanceof Tradable) {
+            return ((Tradable) item).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
